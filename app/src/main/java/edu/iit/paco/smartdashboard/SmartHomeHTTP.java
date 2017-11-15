658b6512ad3de73c45aee97467926ada1399818e
pacoard <pacoard@gmail.com>
2017-11-15 00:37:13 -0600

    Successfull HTTP retrieval of a JSON file and display it in the UI with AsyncTask

---
+++ app/src/main/java/edu/iit/paco/smartdashboard/SmartHomeHTTP.java
@@ -0,0 +1,124 @@
+package edu.iit.paco.smartdashboard;
+
+import android.os.Handler;
+import android.util.JsonReader;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+
+
+/**
+ * Interface with the server
+ *
+ * https://www.learn2crack.com/2013/10/android-asynctask-json-parsing-example.html
+ * https://developer.android.com/reference/android/os/AsyncTask.html
+ */
+
+/* // How to use SmartHomeHTTP to update UI
+ handler.post(new Runnable() {
+    @Override
+    public void run() {
+        SmartHomeHTTP sh = new SmartHomeHTTP("https://api.github.com/");
+        String temp = sh.getTemperature();
+        textView.setText(temp);
+    }
+});
+*/
+public class SmartHomeHTTP {
+
+    private String homeURL;
+
+    public SmartHomeHTTP(String homeURL) {
+        this.homeURL = homeURL;
+    }
+
+    public String getTemperature() {
+        String t = "";
+        SmartHomeHTTPAsyncTask shat = new SmartHomeHTTPAsyncTask();
+        try {
+            // t = shat.execute(homeURL + "/temp").get();
+            t = shat.execute(homeURL).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+    private class SmartHomeHTTPAsyncTask extends AsyncTask<String,Void,String>{
+        private URL homeURL;
+        private HttpURLConnection conn;
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "";
+            URL homeURL;
+           /* try {
+                homeURL = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }*/
+            try {
+                homeURL = new URL(params[0]);
+                conn = (HttpURLConnection) homeURL.openConnection();
+                if (conn.getResponseCode() == 200) {
+                    // Success
+                    InputStream respInputStream = conn.getInputStream();
+                    InputStreamReader respInputStreamReader = new InputStreamReader(respInputStream, "UTF-8");
+                    //Get raw response body
+
+                    BufferedReader br = new BufferedReader(respInputStreamReader);
+                    StringBuilder sb = new StringBuilder();
+                    String output;
+                    while ((output = br.readLine()) != null) {
+                        sb.append(output);
+                    }
+                    result = sb.toString();
+
+
+                    //JSON parsing
+                    /*JsonReader jsonReader = new JsonReader(respBodyReader);
+                    jsonReader.beginObject(); // Start processing the JSON object
+                    while (jsonReader.hasNext()) { // Loop through all keys
+                        String key = jsonReader.nextName(); // Fetch the next key
+                        if (key.equals("organization_url")) { // Check if desired key
+                            // Fetch the value as a String
+                            result = jsonReader.nextString();
+
+                            // Do something with the value
+
+                            break; // Break out of the loop
+                        } else {
+                            jsonReader.skipValue(); // Skip values of other keys
+                        }
+                    }
+                    jsonReader.close();
+                    */
+                    conn.disconnect();
+                } else {
+                    // Error handling
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+    }
+}
+
+
