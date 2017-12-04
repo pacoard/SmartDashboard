702098ffcad69ec694ba19d743535943e663c4db
pacoard <pacoard@gmail.com>
2017-12-04 13:24:20 -0600

    Remove unnecesarry imports

--- app/src/main/java/edu/iit/paco/smartdashboard/LoginActivity.java
+++ app/src/main/java/edu/iit/paco/smartdashboard/LoginActivity.java
@@ -1,23 +1,19 @@
 package edu.iit.paco.smartdashboard;
 
-//package com.sourcey.materiallogindemo;
 
-        import android.app.AlarmManager;
-        import android.app.PendingIntent;
-        import android.app.ProgressDialog;
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.util.Log;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import android.content.Intent;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
-
-        import butterknife.ButterKnife;
-        import butterknife.InjectView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
